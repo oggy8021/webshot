b14202b79c9599dc1932f0cf1006a2535646685c
oggy8021 <oggyist@gmail.com>
2010-07-31 16:36:52 +0000

    git-svn-id: file:///home/katsu/svn/webshot/trunk@12 7d59c931-7d51-42d6-a56e-1e7f43148464

--- shottest.php
+++ shottest.php
@@ -2,7 +2,8 @@
 
 require 'webshot.php';
 
-webshot("http://www.yahoo.co.jp"); 
-webshot("http://www.goo.ne.jp"); 
+webshot("http://www.yahoo.co.jp");
+print "<hr>";
+webshot("http://www.goo.ne.jp");
 
 ?>
