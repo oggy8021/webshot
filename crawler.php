2345b6e737abd09f3090109f5370a64157a2a7db
oggy8021 <oggyist@gmail.com>
2010-08-08 13:58:21 +0000

    git-svn-id: file:///home/katsu/svn/webshot/trunk@23 7d59c931-7d51-42d6-a56e-1e7f43148464

--- crawler.php
+++ crawler.php
@@ -5,6 +5,12 @@
 require 'debug.php';
 require 'shotdb.php';
 
+$display = ':5';
+$profile = 'webshot';
+$firefox = '/usr/bin/firefox';
+$import = '/usr/local/bin/import';
+$cachedir = '/var/www/html/image/webshot';
+
 $ret = null;
 
 //dbが存在するかチェック
@@ -29,29 +35,57 @@ if (! EnableShotTab() )
 
 } else {
 
+	//仮想画面でFirefoxを起動する
+	$cmd = '/bin/ps -ef | grep "/usr/lib/firefox" | grep "display :5"';
+	$lastcon = exec($cmd, $ret);
+	if ($ret)
+	{
+		$cmd = $firefox . ' -UILocale ja -display ' . $display . ' -width 800 -height 600 -p "' . $profile . '" >/dev/null &';
+		passthru($cmd, $ret);
+		sleep(15);
+		if ($ret)
+		{
+			debugCon("Failed Running Firefox (ret = $ret)");
+			return FALSE;
+		}
+	}
+
 	$resultset = SearchShotTabGetFlag();
 
 	foreach ($resultset as $row => $rec)
 	{
-		print $rec["md5"] . $rec["flag"] . $rec["ins_date"] . $rec["shot_date"] . $rec["url"] . "\n";
-
-		//仮想画面でFirefoxを起動する
-		$cmd = 'ps -ef | grep "/usr/lib/firefox" | grep "display :5"';
-		system($cmd, $ret);
-		if (! $ret) {
-			$cmd = '/usr/lib/firefox-3.6/firefox -UILocale ja -display :5 -width 800 -height 600 -p "webshot" &';
-			system($cmd, $ret);
-			if (! $ret) {
-				debugCon('Failed Running Firefox');
-				return FALSE;
-			}
-		}
+		//print $rec["md5"] . $rec["flag"] . $rec["ins_date"] . $rec["shot_date"] . $rec["url"] . "\n";
 
 		//対象のURLにアクセスする
+		$cmd = $firefox .' -display ' . $display . ' -remote "openurl(' . $rec["url"] . ')" >/dev/null &';
+		$lastcon = exec($cmd, $ret);
+		if ($ret)
+		{
+			debugCon('Failed Open Url (url = ' . $rec["url"] . ')');
+		} else {
+			sleep(10);
+		}
+
 		//Image::Magickでスクリーンショットを撮る
+		$cmd = $import .' -display ' . $display . ' -window root ' . $cachedir . '/' . $rec["md5"] . '.jpg';
+		$lastcon = exec($cmd, $ret);
+		if ($ret)
+		{
+			debugCon('Failed Snapshot (url = ' . $rec["url"] . ')');
+		}
+
 		//撮った画像を縮小する
+		//$shotimg = new Imagick($cachedir . '/' . $rec["md5"] . '.jpg');
+		//$shotimg->thumbnailImage(400, 300, true);
+
 		//shotdb更新
+		
+
+		debugCon($cmd);
+		//$cmd = $firefox .' -display ' . $display . ' -remote "openurl(about:blank)" &';
+		return TRUE;
 	}
+	return TRUE;
 }
 
 CloseShotDb();
