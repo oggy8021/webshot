97ab2c460f483723647fbf0fa9192ad075deb1cd
oggy8021 <oggyist@gmail.com>
2010-08-15 03:10:08 +0000

    git-svn-id: file:///home/katsu/svn/webshot/trunk@25 7d59c931-7d51-42d6-a56e-1e7f43148464

--- imagick_test.php
+++ imagick_test.php
@@ -3,21 +3,18 @@
 require 'debug.php';
 
 $shotimg = new Imagick();
-$shotimg->readImage('/var/www/html/image/webshot/org.jpg');
-debugCon('1');
+$shotimg->readImage('/var/www/html/image/webshot/org.png');
+$shotimg->thumbnailImage(200, 150);
+$shotimg->roundCorners(5, 5);
+$shadow = $shotimg->clone();
+$shadow->setImageBackgroundColor( new ImagickPixel('black') );
+//$shadow->shadowImage(80, 3, 5, 5);
+$shadow->shadowImage(20, 3, 5, 5);
+//$shadow->writeImage('/var/www/html/image/webshot/shd.png');
+$shadow->compositeImage($shotimg, Imagick::COMPOSITE_OVER, 0, 0);
+$shadow->writeImage('/var/www/html/image/webshot/org2.png');
 
-//$shotimg->implodeImage;
-//$shotimg->resizeImage(400, 300, imagick::FILTER_MITCHELL, false);
-$shotimg->thumbnailImage(400, 300);
-$shotimg->roundCorners(20, 10);
-//$colors = $shotimg->getImageColors();
-//debugCon("$colors");
-debugCon('2');
-
-$shotimg->writeImage('/var/www/html/image/webshot/org.jpg');
-debugCon('3');
-
-//$shotimg->destroy();
-//debugCon('4');
+$shotimg->destroy();
+$shadow->destroy();
 
 ?>
